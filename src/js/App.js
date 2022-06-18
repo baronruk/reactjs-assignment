5ce62a40e2c95962f50486813861314979028063
baronruk <79830650+baronruk@users.noreply.github.com>
2022-06-18 13:03:06 +0300

    create basic page layout

---
+++ src/js/App.js
@@ -0,0 +1,20 @@
+import Container from '@mui/material/Container';
+import ProductCard from './components/ProductCard';
+import Grid from '@mui/material/Grid';
+
+function App() {
+    return (
+        <div className='App'>
+            <Container>
+                <Grid container spacing={5}>
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </Grid>
+            </Container>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/js/components/ProductCard.js
@@ -0,0 +1,10 @@
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+export default function ProductCard() {
+    return (
+        <Grid item xs={3}>
+            <Paper elevation={3}>Lorem Ipsum</Paper>
+        </Grid>
+    );
+}
