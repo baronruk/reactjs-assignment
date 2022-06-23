44da7db2a5f308b5fac8803fb1faaa3b7ddd89f1
baronruk <79830650+baronruk@users.noreply.github.com>
2022-06-23 08:08:24 +0300

    extract Home,js

--- src/js/App.js
+++ src/js/App.js
@@ -1,33 +1,9 @@
 import '../css/App.css';
 
-import { Container, Grid, Typography } from '@mui/material';
-
-import ProductCard from './components/ProductCard';
-import SearchAppBar from './components/AppBar';
-import categories from '../data.json';
+import Home from './pages/Home';
 
 function App() {
-    return (
-        <div className='App'>
-            <SearchAppBar />
-            <Container sx={{ marginY: 5 }}>
-                {categories.map(category => {
-                    return (
-                        <>
-                            <Typography variant='h4' component='h2' marginBottom={3} marginTop={5}>
-                                Most Popular {category.category}
-                            </Typography>
-                            <Grid container spacing={4}>
-                                {category.products.map(product => {
-                                    return <ProductCard product={product} />;
-                                })}
-                            </Grid>
-                        </>
-                    );
-                })}
-            </Container>
-        </div>
-    );
+    return <Home />;
 }
 
 export default App;
---
+++ src/js/pages/Home.js
@@ -0,0 +1,29 @@
+import { Container, Grid, Typography } from '@mui/material';
+
+import ProductCard from '../components/ProductCard';
+import SearchAppBar from '../components/AppBar';
+import categories from '../../data.json';
+
+export default function Home() {
+    return (
+        <div className='App'>
+            <SearchAppBar />
+            <Container sx={{ marginY: 5 }}>
+                {categories.map(category => {
+                    return (
+                        <>
+                            <Typography variant='h4' component='h2' marginBottom={3} marginTop={5}>
+                                Most Popular {category.category}
+                            </Typography>
+                            <Grid container spacing={4}>
+                                {category.products.map(product => {
+                                    return <ProductCard product={product} />;
+                                })}
+                            </Grid>
+                        </>
+                    );
+                })}
+            </Container>
+        </div>
+    );
+}
