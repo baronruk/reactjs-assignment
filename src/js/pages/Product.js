03461c3a78d47f05ffc0041c3a05681f11c03a2c
baronruk <79830650+baronruk@users.noreply.github.com>
2022-06-23 09:45:50 +0300

    Update Product.js

--- src/js/pages/Product.js
+++ src/js/pages/Product.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Container, Typography, Box } from '@mui/material';
+import FAQAccordions from '../components/FAQaccordion';
+import FooterNavigation from '../components/FooterNavigation';
 
 export default function Product() {
-    return <div>placeholder</div>;
+    return (
+        <Container sx={{ width: 900, marginY: 5 }}>
+            <Typography variant='h3' component='h1' marginTop={3}>
+                Product Placeholder Page
+            </Typography>
+            <Box marginTop={3} display='flex' justifyContent='center' alignItems='center'>
+                <img src='./media/images/placeholder.png' height={325} />
+            </Box>
+            <Typography variant='h6' component='h4' marginTop={3}>
+                About this Product
+            </Typography>
+            <Box sx={{ display: 'flex' }}>
+                <Typography variant='paragraph' component='p' marginY={3}>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit voluptates rem
+                    quos delectus debitis earum modi, ipsum veritatis. Perferendis officiis nobis,
+                    aut hic praesentium nulla vero, possimus omnis reprehenderit blanditiis quis
+                    incidunt minima voluptatibus illum quam corporis libero fugiat doloremque. Lorem
+                    ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem officiis
+                    commodi beatae animi incidunt necessitatibus aut provident ad ex. Saepe!
+                </Typography>
+            </Box>
+            <Typography variant='h6' component='h4' marginBottom={3}>
+                Frequently Asked Questions
+            </Typography>
+            <FAQAccordions />
+            <FooterNavigation />
+        </Container>
+    );
 }
